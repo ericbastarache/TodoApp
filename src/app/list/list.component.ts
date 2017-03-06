ad370e0f4961ed3112807d14667f878b73979747
ericb <eric_bastarache@hotmail.com>
2017-03-06 02:23:51 -0500

    Updated list component styling

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+  newItem = 'test';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem () {
+
   }
 
 }
